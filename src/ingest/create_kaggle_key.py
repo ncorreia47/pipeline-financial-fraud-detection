99f68db5038cb1501c81338a8be1dae220f1ba51
ncorreia47 <dev.ncorr@gmail.com>
2025-05-20 21:22:04 -0300

    update: desenvolvido metodo estatico; aplicado controle de exceções e configs de logs

--- src/ingest/create_kaggle_key.py
+++ src/ingest/create_kaggle_key.py
@@ -1,21 +1,51 @@
 import os
 import json
 from dotenv import load_dotenv
+from colorama import init, Fore, Style
 
-# Carrega as variáveis do arquivo .env
-load_dotenv()
+# Customização de saídas nos prompts de terminais
+init(autoreset=True)
 
-# Caminho para onde você salvou o kaggle.json
-kaggle_user = os.getenv("KAGGLE_USER")
-kaggle_key = os.getenv("KAGGLE_KEY")
-kaggle_api_token = {"username": kaggle_user,"key":kaggle_key}
+class CreateKaggleKey:
+    """
+    Classe responsável por criar o arquivo de configurações do kaggle
+    """
 
-# Cria a pasta ~/.kaggle se não existir
-os.makedirs(os.path.expanduser("~/.kaggle"), exist_ok=True)
+    @staticmethod
+    def create_kaggle_key() -> None:
+        
+        # Carrega as variáveis do arquivo .env
+        load_dotenv()
+        kaggle_user = os.getenv("KAGGLE_USER")
+        kaggle_key = os.getenv("KAGGLE_KEY")
+        kaggle_url = os.getenv("KAGGLE_URL")
+        kaggle_file = os.getenv("KAGGLE_FILE")
+        kaggle_api_token = {"username": kaggle_user,"key": kaggle_key}
+        kaggle_path = os.path.expanduser(f'{kaggle_url}')
+        kaggle_file_path = os.path.join(kaggle_path, kaggle_file)
 
-# Salva o arquivo
-with open(os.path.expanduser("~/.kaggle/kaggle.json"), "w") as f:
-    json.dump(kaggle_api_token, f)
+        try:
+            # Cria a pasta ~/.kaggle se não existir
+            os.makedirs(kaggle_path, exist_ok=True)
 
-# Garante as permissões corretas (necessário em alguns sistemas)
-os.chmod(os.path.expanduser("~/.kaggle/kaggle.json"), 0o600)
+            # Salva o arquivo
+            with open(kaggle_file_path, "w") as f:
+                json.dump(kaggle_api_token, f)
+            
+            print(Style.BRIGHT + Fore.GREEN + 'Arquivo criado com sucesso!')
+
+            try:
+                # Garante as permissões corretas (necessário em alguns sistemas)
+                os.chmod(kaggle_file_path, 0o600)
+
+            except Exception as permission_error:
+                print(Style.BRIGHT + Fore.RED+ 'Erro ao atualizar permissões!')
+                raise permission_error
+
+        except Exception as create_file_error:
+            print(Style.BRIGHT + Fore.RED+ 'Erro ao criar o arquivo!')
+            raise create_file_error
+
+        
+if __name__ == '__main__':
+    CreateKaggleKey.create_kaggle_key()
