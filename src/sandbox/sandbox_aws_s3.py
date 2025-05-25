f7a7a9162501fdce58bbe8888bd94c8ab4fe8da9
ncorreia47 <dev.ncorr@gmail.com>
2025-05-25 16:10:01 -0300

    fix: ajustado processo de upload no s3 e implementado tratamento de exceções

--- src/sandbox/sandbox_aws_s3.py
+++ src/sandbox/sandbox_aws_s3.py
@@ -1,21 +1,38 @@
 import boto3
 import os
 from dotenv import load_dotenv
+from src.utils.custom_logger import BrightYellowPrint, RedBoldPrint, GreenNormalPrint, Printer
 
 class AWSS3Connection:
-    
+
+    def __init__(self, printer: Printer):
+        self.printer = printer
+        load_dotenv()
+
+
     def create_s3_connection(self):
         
-        load_dotenv()
         aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
         aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-        s3_client = boto3.client(
-            's3', 
-            aws_access_key_id=aws_access_key_id, 
-            aws_secret_access_key=aws_secret_access_key
-            )
 
-        return s3_client
+        self.printer.set_strategy(BrightYellowPrint())
+        self.printer.display('Criando a conexão com S3')
+        try:
+            s3_client = boto3.client(
+                's3', 
+                aws_access_key_id=aws_access_key_id, 
+                aws_secret_access_key=aws_secret_access_key
+                )
+        
+            self.printer.set_strategy(GreenNormalPrint())
+            self.printer.display('Conexão com S3 criada com sucesso!')
+
+            return s3_client
+        
+        except Exception as create_s3_connection_error:
+            self.printer.set_strategy(RedBoldPrint())
+            self.printer.display('Problemas ao criar a conexão com S3')
+            raise create_s3_connection_error 
     
 
     def upload_to_s3(self):
@@ -23,21 +40,22 @@ class AWSS3Connection:
         Faz upload de um arquivo para um bucket S3.
         """
         # Inicializa o cliente S3
-        try:
-            s3_client = self.create_s3_connection()
+        s3_client = self.create_s3_connection()
         
-        except Exception as create_s3_connection_error:
-            raise create_s3_connection_error
-
         # Carrega as variáveis de ambiente
-        load_dotenv()
         bucket_name = os.getenv("AWS_BUCKET_NAME")
         dataset_local_path = os.getenv("DATASET_LOCAL_PATH")
         file_name = os.getenv("FILE_NAME")
         file_path = os.path.join(dataset_local_path, file_name)
         
+        self.printer.set_strategy(BrightYellowPrint())
+        self.printer.display('Iniciando o processo de upload do arquivo para o S3. Isso pode demorar um pouco')
         try:
             s3_client.upload_file(file_path, bucket_name, file_name)
-            print(f"Arquivo {file_name} enviado com sucesso para s3://{bucket_name}")
-        except Exception as e:
-            print(f"Erro ao enviar o arquivo: {e}")
+            self.printer.set_strategy(GreenNormalPrint())
+            self.printer.display(f'Arquivo {file_name} enviado com sucesso para s3://{bucket_name}')
+
+        except Exception as upload_file_error:
+            self.printer.set_strategy(RedBoldPrint())
+            self.printer.display(f'Arquivo {file_name} não foi enviado para s3://{bucket_name}')
+            raise upload_file_error
